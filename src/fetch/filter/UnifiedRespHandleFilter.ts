ebcc24120c664c6fa9cbb11f98ced937fc20a8e9
风 <1109695647@qq.com>
2018-05-28 17:24:32 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- src/fetch/filter/UnifiedRespHandleFilter.ts
+++ src/fetch/filter/UnifiedRespHandleFilter.ts
@@ -1,7 +1,6 @@
 import ApiAbstractFilter from "typescript_api_sdk/src/api/filter/ApiAbstractFilter"
 import {ApiResp} from "typescript_api_sdk/src/api/model/ApiResp";
-import {isNullOrUndefined} from "util";
-import {message as AntdMessage} from "antd";
+import {Toast} from "antd-mobile";
 import {FetchOption} from "typescript_api_sdk/src/api/option/FetchOption";
 
 
@@ -10,44 +9,23 @@ let count = 0;
 /**
  * 统一响应处理过滤器
  */
-export class UnifiedRespHandleFilter extends ApiAbstractFilter<FetchOption,ApiResp<any>> {
+export class UnifiedRespHandleFilter extends ApiAbstractFilter<FetchOption, ApiResp<any>> {
 
-
-    // preHandle(params: FetchOption): boolean | Promise<boolean> {
-    //
-    //     const {headers} = params;
-    //     params.headers = {
-    //         ...headers,
-    //         viewType: 'react'
-    //     };
-    //     return true;
-    // }
 
     postHandle(resp: ApiResp<any>, context?: any): boolean {
         const {message, success, actions} = resp;
 
-        if ('records' in resp) {
-            //返回列表数据的请求
+        if (success) {
             return true;
         }
-        if (success) {
-            AntdMessage.success(message ? message : "操作成功", 2, () => {
-                if (isNullOrUndefined(actions) || actions.length === 0) {
-                    return;
-                }
-                // analysisAction(actions[0]);
-            });
-        } else {
-            if (count === 0) {
-                //请求失败
-                count++;
-                AntdMessage.warn(message ? message : "操作失败", 2, () => {
-                    count--;
-                });
-            }
-
+        if (count > 0) {
+            return false;
         }
-
-        return true;
+        //请求失败
+        count++;
+        Toast.info(message ? message : "操作失败", 2, () => {
+            count--;
+        });
+        return false;
     }
 }
