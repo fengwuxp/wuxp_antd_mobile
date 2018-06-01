a15e7d677e9f751385abc13f0065e644606dbd58
风 <1109695647@qq.com>
2018-06-01 11:31:02 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ src/layouts/view/AntdAbstractRefreshView.tsx
@@ -0,0 +1,54 @@
+import AbstractSimpleQueryView, {SimpleQueryViewState} from "wuxp_react_dynamic_router/src/layout/view/AbstractSimpleQueryView";
+import {ViewProps} from "wuxp_react_dynamic_router/src/layout/view/AbstractSimpleView";
+import {ApiQueryReq} from "typescript_api_sdk/src/api/model/ApiQueryReq";
+import * as React from "react";
+import {PullToRefresh} from "antd-mobile";
+import {Indicator} from 'rmc-pull-to-refresh/lib/PropsType';
+
+export interface AntdAbstractQueryViewProps extends ViewProps {
+
+}
+
+export interface AntdAbstractQueryViewState extends SimpleQueryViewState {
+
+    refreshing: boolean;
+}
+
+const TPullToRefresh = PullToRefresh as any;
+
+
+export default abstract class AntdAbstractQueryView<Q extends ApiQueryReq, E,
+    P extends AntdAbstractQueryViewProps,
+    S extends AntdAbstractQueryViewState> extends AbstractSimpleQueryView<Q, E, P, S> {
+
+    protected pullToRefresh;
+
+    constructor(props: P, context: any, isPaging: boolean) {
+        super(props, context, isPaging);
+    }
+
+
+    protected renderWrapper = (childrn: React.ReactNode) => {
+        return <TPullToRefresh direction={"down"}
+                               distanceToRefresh={25}
+                               damping={60}
+                               style={{height: "100%", overflowY: 'scroll'}}
+                               ref={el => this.pullToRefresh = el}
+                               indicator={this.getIndicator}
+                               refreshing={this.state.refreshing}
+                               onRefresh={this.onRefresh}>{childrn}</TPullToRefresh>
+    };
+
+    getIndicator = ():Indicator=> {
+        return {
+            activate: "松开刷新",
+            deactivate: '上拉可以刷新',
+            release: "--",
+            finish: "刷新完成"
+        }
+    };
+
+    getScrollContainer = (): React.ReactNode => null;
+
+    abstract onRefresh: () => void;
+}
