6be1cfd0fe40edbbb140b0586c852bdff1e66eb6
风 <1109695647@qq.com>
2018-10-29 14:23:46 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- src/layouts/view/AntdAbstractView.tsx
+++ src/layouts/view/AntdAbstractView.tsx
@@ -9,6 +9,7 @@ import * as React from "react";
 
 export interface AntdAbstractViewProps extends ViewProps {
 
+
 }
 
 export interface AntdAbstractViewState extends ViewState {
@@ -21,14 +22,24 @@ export class AntdViewRenderHelper {
 
     renderHeader = (title: string, p: NavBarProps): React.ReactNode => {
 
+        const pathname = location.pathname;
+        //第一次进入应用程序是的页面
+        if (window['firstViewPathName'] == null) {
+            window['firstViewPathName'] = pathname;
+        }
+
+        const isFirstView = pathname === window['firstViewPathName'];
+
         const props: NavBarProps = {
-            icon: window.history.length >= 2 ? <Icon type="left" size={"lg"}/> : null,
+            icon: isFirstView ? null : <Icon type="left" size={"lg"}/>,
             onLeftClick: () => window.history.back(),
             mode: p.mode || "dark",
             ...p
         };
 
-        return <NavBar {...props as any}><div>{title}</div></NavBar>;
+        return <NavBar {...props as any}>
+            <div>{title}</div>
+        </NavBar>;
     };
 
 
